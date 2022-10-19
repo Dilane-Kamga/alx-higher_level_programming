761d90b86528fb7cbe0dbf949ee8ad4a3f598ae5
Dilane-Kamga <dilanekamga777@hotmail.com>
2022-10-19 10:15:11 +0100

    webscraping js update

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,25 +1,21 @@
 #!/usr/bin/node
-// A script that prints the number of movies where the character Wedge Antilles is present
 
-const args = process.argv;
-let reqURL = args[2];
-let request = require('request');
-request(reqURL, function (error, response, body) {
-  if (error) {
-    console.log('error:', error); // Print the error if one occurred
-  } else {
-    let jso = JSON.parse(body);
-    let results = jso['results'];
-    let count = 0;
-    for (let i = 0; i < results.length; i++) {
-      let chars = (results[i]['characters']);
-      for (let j = 0; j < chars.length; j++) {
-        let check18 = chars[j].endsWith('18/');
-        if (check18) {
-          count++;
-        }
+const request = require('request');
+
+request(process.argv[2], (err, response, body) => {
+  if (err) {
+    console.log(err);
+  }
+
+  const results = JSON.parse(body).results;
+  let count = 0;
+
+  for (const result of results) {
+    for (const res of result.characters) {
+      if (res.endsWith('/18/')) {
+        count++;
       }
     }
-    console.log(count);
   }
+  console.log(count);
 });
